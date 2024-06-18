fd57da76485b22c7045b8d402a83c7fe621f3417
ruffy777 <raphael.pfeffinger@gmail.com>
2024-06-18 09:46:49 +0200

    Create Visual Studio Project
    create API Project
    create Web Project
    Add the documentation Files to the repo for backup safety

---
+++ seclogin/seclogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace seclogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
